17d37e09f1da1ff8f146dcdff8673722ee097711
Daniel Correia <dannielsantoscorreia@gmail.com>
2025-05-31 19:45:02 -0300

    correcao da tela de registrar entrega

--- src/assets/js/movimentacoes-epi.js
+++
@@ -1,6 +0,0 @@
-    // JS opcional para disparar ação ao mudar mês
-    document.getElementById('mesSelect').addEventListener('change', function () {
-      const mesSelecionado = this.value;
-      console.log("Mês selecionado:", mesSelecionado);
-      // Aqui você pode chamar o back-end para atualizar os dados
-    });
