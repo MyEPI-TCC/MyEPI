e29312e98d326618dec25f79ecefd5413940a84c
Daniel Correia <dannielsantoscorreia@gmail.com>
2025-06-05 02:39:16 -0300

    apontamento para producao

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://my-epi-api.vercel.app/';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://my-epi-api.vercel.app/';
+// const API_BASE_URL = 'http://localhost:3000/api';
 
 async function get(endpoint) {
     try {
